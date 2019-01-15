c177dd559978a0372d9bb4276977c32516a92437
Jackson-Kim <43841846+Jackson-Kim@users.noreply.github.com>
2019-01-15 15:24:15 +0900

    20190115-2

--- part2_4/src/main/java/org/techtown/part2_4/MainActivity.java
+++ part2_4/src/main/java/org/techtown/part2_4/MainActivity.java
@@ -1,7 +1,10 @@
 package org.techtown.part2_4;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView)findViewById(R.id.jack);
+        iv.setBackground(new ShapeDrawable(new OvalShape()));
+        iv.setClipToOutline(true);
+
+
+
+
     }
 }
